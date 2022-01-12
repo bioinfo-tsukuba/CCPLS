adeb42240f83e8cb37c1673d2b48a9614eace9e7
t-takaho <takaho.tsuchiya@gmail.com>
2022-01-12 13:25:19 +0900

    Add

---
+++ R/cellCellRegEst.R
@@ -0,0 +1,218 @@
+#' Perform PLS regression
+#'
+#' @param data_4_pls A data frame.
+#' @param cv_opt A string.
+#' @param cv_num A number.
+#' @param component_num A number.
+#'
+#' @importFrom stats var
+#' @importFrom pls plsr
+#'
+buildModel <- function(data_4_pls,
+                       cv_opt = c("CV", "LOOCV")[1],
+                       cv_num = 10,
+                       component_num = NULL){
+
+  if (cv_opt == "CV"){
+
+    if (is.null(component_num)){
+      res_pls <- plsr(gene ~ feature, data = data_4_pls,
+                      ncomp = qr(data_4_pls$feature)$rank,
+                      scale = TRUE,
+                      validation = "CV", segments = cv_num)
+    } else {
+      res_pls <- plsr(gene ~ feature, data = data_4_pls,
+                      ncomp = component_num,
+                      scale = TRUE,
+                      validation = "CV", segments = cv_num)
+    }
+
+    RMSE_cv_all <- sqrt(res_pls$validation$PRESS / nrow(data_4_pls$feature))
+    RMSE_cv_all_sum <- colSums(RMSE_cv_all)
+
+    opt_comp <- RMSE_cv_all_sum[RMSE_cv_all_sum == min(RMSE_cv_all_sum)] %>% names()
+    opt_comp_num <- strsplit(opt_comp, split = " ")[[1]][1] %>% as.double()
+
+  } else if (cv_opt == "LOOCV") {
+
+    if (is.null(component_num)){
+      res_pls <- plsr(gene ~ feature, data = data_4_pls,
+                      ncomp = qr(data_4_pls$feature)$rank,
+                      scale = TRUE,
+                      validation = "LOO")
+    } else {
+      res_pls <- plsr(gene ~ feature, data = data_4_pls,
+                      ncomp = component_num,
+                      scale = TRUE,
+                      validation = "LOO")
+    }
+
+    RMSE_cv_all <- sqrt(res_pls$validation$PRESS / nrow(data_4_pls$feature))
+    RMSE_cv_all_sum <- colSums(RMSE_cv_all)
+
+    opt_comp <- RMSE_cv_all_sum[RMSE_cv_all_sum == min(RMSE_cv_all_sum)] %>% names()
+    opt_comp_num <- strsplit(opt_comp, split = " ")[[1]][1] %>% as.double()
+
+  }
+
+  return(list(res_pls = res_pls,
+              opt_comp_num = opt_comp_num,
+              RMSE_cv_all = RMSE_cv_all,
+              RMSE_cv_all_sum = RMSE_cv_all_sum))
+
+}
+
+#' Estimate PLS regression model
+#'
+#' @param fet_mat_orig A data frame.
+#' @param annot_score_mat A data frame.
+#' @param exp_mat_sep_list A list.
+#' @param fet_mat_sep_list A list.
+#' @param estimate_cell_type_opt A string.
+#' @param cell_type_list A string.
+#' @param perm_opt A logical.
+#' @param component_num_list tentative.
+#' @param dev_opt A string.
+#'
+#'
+cellCellRegEst <- function(fet_mat_orig,
+                           annot_score_mat,
+                           exp_mat_sep_list,
+                           fet_mat_sep_list,
+                           estimate_cell_type_opt = "all",
+                           cell_type_list,
+                           perm_opt = FALSE,
+                           component_num_list = NULL,
+                           dev_opt = "pls"
+){
+
+  start_time <- Sys.time()
+  print(paste0("=== cellCellRegEst started... ", Sys.time(), " ==="))
+
+  # prepare function
+  beera <- function(expr){
+    tryCatch(expr,
+             error = function(e){
+               # message("An error occurred:\n", e)
+               return("An error occurred")
+             })
+  }
+
+  target_cell_type_index <- 0
+  gene_list <- list()
+
+  ## choose column index for estimating model
+  if (estimate_cell_type_opt == "all"){
+
+    cell_type_model_SG <- c()
+
+    # Get "self"
+    self_col_index <- grep("self", colnames(fet_mat_orig))
+    estimate_col_index <- seq(1:length(cell_type_list))
+
+    component_num_list <- rep(0, length = length(estimate_col_index))
+    estimate_cell_type_list <- cell_type_list
+
+  } else {
+
+    cell_type_model_SG <- estimate_cell_type_opt
+
+    # Get "self"
+    self_col_index <- grep("self", colnames(fet_mat_orig))
+    estimate_col_index <- rep(0, length(cell_type_model_SG))
+
+    for(estimate_num in 1:length(cell_type_model_SG)){
+      estimate_col_index[estimate_num] <- grep(cell_type_model_SG[estimate_num], cell_type_list)
+    }
+
+    estimate_cell_type_list <- cell_type_list[estimate_col_index]
+
+  }
+
+  estimate_col_num <- length(estimate_col_index)
+  self_cell_type_list <- cell_type_list
+
+  CCPLS_result <- vector("list", estimate_col_num)
+
+  # For each "self" PLS regression
+  for (i in 1:estimate_col_num){
+
+    print(paste0("Under calculation in cell type ", self_cell_type_list[estimate_col_index[i]], "..."))
+
+    # Get each "self"
+    fet_mat_sep <- as.matrix(fet_mat_sep_list[[estimate_col_index[i]]])
+    exp_mat_sep <- as.matrix(exp_mat_sep_list[[estimate_col_index[i]]])
+
+    fet_mat_sep_rem <- fet_mat_sep[, - self_col_index]
+
+    # Remove feature with variance zero
+    fet_mat_sep_rem <- fet_mat_sep_rem[,apply(fet_mat_sep_rem, 2, stats::var) != 0]
+
+    fet_mat_sep_rem_norm <- scale(fet_mat_sep_rem, center = TRUE, scale = TRUE) %>% as.data.frame()
+    exp_mat_sep_norm <- scale(exp_mat_sep, center = TRUE, scale = TRUE) %>% as.data.frame()
+
+    # plsr
+    data_4_pls <- list()
+    data_4_pls$feature <- as.matrix(fet_mat_sep_rem_norm)
+    data_4_pls$gene<- as.matrix(exp_mat_sep_norm)
+
+    if ((length(data_4_pls$feature) != 0) && (!is.na(data_4_pls$gene)[1]) && (colnames(data_4_pls$gene) != "NULL")){
+
+      returned_value <- buildModel(data_4_pls = data_4_pls,
+                                   cv_opt = c("CV", "LOOCV")[1],
+                                   cv_num = 10)
+
+      component_num_list[i] <- returned_value$opt_comp_num
+
+    } else {
+
+      returned_value <- NULL
+
+      CCPLS_result[[i]] <- list(paste0("No model was built."),
+                                paste0("Model content is empty."))
+
+    }
+
+    if ((length(data_4_pls$feature != 0)) && (!is.na(data_4_pls$gene)[1]) && (colnames(data_4_pls$gene) != "NULL")){
+      target_cell_type_index <- target_cell_type_index + 1
+
+      CCPLS_result[[i]] <- list(paste0(self_cell_type_list[estimate_col_index[i]]),
+                                returned_value)
+
+      gene_list[[target_cell_type_index]] <- colnames(exp_mat_sep_norm)
+
+    } else {
+
+      CCPLS_result[[i]] <- list(paste0("No model was built."),
+                                paste0("Model content is empty."))
+
+    }
+
+  }
+
+  for (j in 1:length(estimate_cell_type_list)){
+
+    judge_model_estimated <- grep(estimate_cell_type_list[j], CCPLS_result[[j]][[1]]) # [[j]][[1]] に細胞型の名前が入っている
+
+    if (length(judge_model_estimated) != 0){
+      cell_type_model_SG <- append(cell_type_model_SG, self_cell_type_list[j])
+    }
+
+  }
+
+  finish_time <- Sys.time()
+
+  finish_time <- Sys.time()
+  print(paste0("=== cellCellRegEst finished... ", Sys.time(), " ==="))
+  print(paste0("=========="))
+
+  return(list(CCPLS_result = CCPLS_result,
+              self_cell_type_list = self_cell_type_list,
+              cell_type_model_SG = cell_type_model_SG,
+              cell_type_list = cell_type_list,
+              gene_list = gene_list,
+              component_num_list = component_num_list,
+              start_time = start_time,
+              finish_time = finish_time))
+
+}
---
+++ README.md
@@ -0,0 +1,48 @@
+# CCPLS <img src="https://user-images.githubusercontent.com/49115350/114253008-345bf600-99e3-11eb-8b62-4af53ed665f9.png" width="30%" align="right" />
+
+R package for estimating cell-cell communications from spatial transcriptome data with single-cell resolution.
+
+Please see preprint of CCPLS for details.
+
+## Getting started
+
+### Install
+
+Requirements: R version 4.2.0 or higher.
+
+```
+# Install dependent packages
+install.packages(c("cluster", "circlize", "dplyr", "pls", "purrr", "Seurat", "stringr"), dependencies = TRUE)
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install("ComplexHeatmap")
+# Install CCPLS
+install.packages("devtools", dependencies = TRUE)
+devtools::install_github("bioinfo-tsukuba/CCPLS")
+ ```
+
+### Quick exmaple
+
+#### 1. Prepare arguments for CCPLS
+
+```
+# Here is demonstraion by included dataset in CCPLS package.
+# Please prepare dataset and output directory for your purpose.
+load(system.file("extdata", "dataset.Rdata", package = "CCPLS"))
+output_dir <- "~/CCPLS_test"
+
+# Note that 400 cells were randomly extracted by Seq-Scope data (Cho et al., 2021), and this demonstration cannot be interpreted biologically.
+```
+
+#### 2. Run CCPLS
+
+```
+result_CCPLS <- CCPLS::cellCellReg(exp_mat, coord_mat, annot_mat, output_dir)
+```
+
+#### 3. View reports by CCPLS
+
+<img src="https://user-images.githubusercontent.com/49115350/148733504-73c78ba4-b8d1-4c31-9026-925c827ae5cb.png" width="50%"><img src="https://user-images.githubusercontent.com/49115350/148733531-6943ef3e-ba43-466b-8177-c8cfb7f2e166.png" width="50%">
+
+## License
+Copyright (c) 2022 Takaho Tsuchiya and Bioinformatics Laboratory, Faculty of Medicne, University of Tsukuba released under the [Artistic License 2.0](http://www.perlfoundation.org/artistic_license_2_0).
